51165831ce15e917433fd81a8e297425b97739cd
Bryan <bolianchen@gmail.com>
2022-11-17 12:27:43 +0800

    Update docker_fastapi_app1

--- docker_fastapi_app1/README.md
+++ docker_fastapi_app1/README.md
@@ -5,3 +5,8 @@ A simple application running in a Docker container accessible by localhost:8000
 ```
 docker-compose -f docker-compose.yaml up --build
 ```
+
+## API
+<p align="center">
+  <img src="api.png" width="600" />
+</p>
--- docker_fastapi_app1/main.py
+++ docker_fastapi_app1/main.py
@@ -2,17 +2,20 @@ from typing import Union
 from fastapi import FastAPI
 app = FastAPI()
 
-# greeting at the root
-@app.get("/")
+@app.get("/", summary="Get the root")
 def read_root():
     return {"Hello": "World"}
 
-# return item information by using path and query parameters
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+    """
+    Get information according to the input item_id
+    """
     return {"item_id": item_id, "q": q}
 
-# feed a text via a path parameter and return words by splitting the text
 @app.get('/tokenizer/{text}')
 def tokenized(text: str):
+    """
+    Get the tokenized result of the input text
+    """
     return text.split()
